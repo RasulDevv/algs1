1727d530dafbc56f05c06d84652544df048365a7
Rasul Rixsiboyev <rasuldevv@gmail.com>
2023-03-05 20:17:01 +0500

    updated

--- exersice_pointer.c
+++ exersice_pointer.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
-void change(int*a, int*b){
-    int r = *a;
-    *a = *b;
-    *b = r;
+void f1(int*);
+void change(int*, int*);
+
+int main() {
+	int n = 5, n1 = 17, n2 = 19;
+	f1(&n);
+	printf("n = %d\n\n", n);
+
+	printf("n1 = %d\nn2 = %d\n\n", n1, n2);
+	change(&n1, &n2);
+	printf("n1 = %d\nn2 = %d\n\n", n1, n2);
 }
 
-int main(){
-    int x = 11, y = 17;
-    change(&x, &y);
-    printf("x = %d\ny = %d", x, y);
-    return 0;
+void f1(int* p) {
+	int x = 7;
+	*p = x;
+}
+
+void change(int *x, int *y) {
+	int r = *x;
+	*x = *y;
+	*y = r;
 }
